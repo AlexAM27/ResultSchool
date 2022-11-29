fe4ff0c415d93c7e37a1b3cb8ec61522c85c6c91
AlexAM27 <alexandermarkin27@gmail.com>
2022-11-29 22:13:15 +0100

    excercise 2

---
+++ Module_19/Excercise_2/rollup.config.js
@@ -0,0 +1,33 @@
+import styles from "rollup-plugin-styles";
+import image from '@rollup/plugin-image';
+import { babel } from '@rollup/plugin-babel';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
+export default {
+    input: './src/index.js',
+    output: {
+      file: './build/bundle.js',
+      format: 'cjs'
+    },
+    watch: {
+      exclude: 'node_modules/**'
+    },
+    plugins: [
+      styles(),
+      image({
+        limit: 1000000,
+      }),
+      babel({ 
+        babelHelpers: 'bundled',
+        presets: ["@babel/env"]
+      }),
+      
+      serve({
+        open: true,
+        contentBase: './',
+        port: '8000',
+      }),
+      livereload(),
+    ],
+  };
