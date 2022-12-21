cc668d4d3789b32b51e86e42c7ca4b69f9ef9ad5
Alexander Markin <alexander.markin@creamteam.de>
2022-12-21 12:02:21 +0100

    render users table

--- React/fast-company/src/components/users.jsx
+++ React/fast-company/src/components/users.jsx
@@ -2,27 +2,62 @@ import React from 'react';
 import { useState } from 'react';
 import api from '../api';
 import { createEndingForUserWord } from '../utils/utils';
+import * as constants from '../utils/constants';
 
 export const Users = () => {
   const usersList = api.users.fetchAll();
   const [usersAmount, setUsersAmount] = useState(usersList.length);
 
-  const renderUsersAmountTag = () => {
-    if(usersAmount <= 0) {
-      return 'Никто с тобой не тусанет';
-    } else {
-      return `${usersAmount} ${createEndingForUserWord(usersAmount)} туанет с тобой сегодня`;
-    }
+  const renderUsersBadge = () => {
+    return `${usersAmount} ${createEndingForUserWord(usersAmount)} ${constants.USERS_LIST_LABEL_ENDING}`;
+  }
+
+  const renderUserQualities = (user) => {
+    return user.qualities.map((quality => (
+      <div key={quality._id}>{quality.name}</div>
+    )))
+  }
+
+  const renderUsersTable = () => {
+    return (usersList.map(user => (
+      <tr key={user._id}>
+        <td>{user.name}</td>
+        <td>{renderUserQualities(user)}</td>
+        <td>{user.profession.name}</td>
+        <td>{user.completedMeetings}</td>
+        <td>{user.rate}</td>
+        <td><button>{constants.DELETE_USER_BUTTON_LABEL}</button></td>
+      </tr>
+    )))
   }
 
   const handleDeleteUsers = () => {
     setUsersAmount((prevState) => prevState - 1);
   }
 
+  if(usersAmount <= 0) {
+    return <span className={constants.RED_BADGE}>{constants.EMPTY_USERS_LIST_LABEL}</span>;
+  }
+
   return (
     <>
-      <span>{renderUsersAmountTag()}</span>
-      <button onClick={handleDeleteUsers}>Delete</button>
+      <span className={constants.BLUE_BADGE}>{renderUsersBadge()}</span>
+
+      <table className='table'>
+        <thead>
+          <tr>
+            <th scope="col">{constants.USERS_TABLE_HEADER_LABEL_NAME}</th>
+            <th scope="col">{constants.USERS_TABLE_HEADER_LABEL_QUALITY}</th>
+            <th scope="col">{constants.USERS_TABLE_HEADER_LABEL_PROFESSION}</th>
+            <th scope="col">{constants.USERS_TABLE_HEADER_LABEL_MEETINGS}</th>
+            <th scope="col">{constants.USERS_TABLE_HEADER_LABEL_RATE}</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderUsersTable()}
+        </tbody>
+      </table>
     </>
   );
 };
---
+++ React/fast-company/src/utils/constants.js
@@ -0,0 +1,17 @@
+export const EMPTY_USERS_LIST_LABEL = 'Никто с тобой не тусанет';
+export const USERS_LIST_LABEL_ENDING = 'туcанет с тобой сегодня';
+
+export const USERS_TABLE_HEADER_LABEL_NAME = 'Имя';
+export const USERS_TABLE_HEADER_LABEL_QUALITY = 'Качество';
+export const USERS_TABLE_HEADER_LABEL_PROFESSION = 'Профессия';
+export const USERS_TABLE_HEADER_LABEL_MEETINGS = 'Встретился, раз';
+export const USERS_TABLE_HEADER_LABEL_RATE = 'Оценка';
+
+export const DELETE_USER_BUTTON_LABEL = 'Удалить';
+
+// BADGE CLASSES
+
+const BADGE = 'badge';
+export const BLUE_BADGE = `${BADGE} text-bg-primary`;
+export const RED_BADGE = `${BADGE} text-bg-danger`;
+
