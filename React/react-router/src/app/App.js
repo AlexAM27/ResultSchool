0c25c8c5a2b7d98f651d871a62635f9e369e7ac0
Alexander Markin <alexander.markin@creamteam.de>
2023-01-23 15:05:13 +0100

    Switch

--- React/react-router/src/app/App.js
+++ React/react-router/src/app/App.js
@@ -1,19 +1,23 @@
 import { NavBar } from "./components/navBar";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Dashboard from "./components/dashboard"
 import Home from "./components/home";
 import Login from "./components/login";
 import Posts from "./components/posts";
+import SomePost from "./components/somePost";
 
 function App() {
   return ( 
     <div>
       <NavBar/>
       <h1>App</h1>
-      <Route path={"/dashboard"} component={Dashboard}/>
-      <Route path={"/"} exact component={Home}/>
-      <Route path={"/login"} component={Login}/>
-      <Route path={"/posts"} component={Posts}/>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/dashboard" component={Dashboard}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/posts/some-post" component={SomePost} />
+        <Route path="/posts" component={Posts}/>
+      </Switch>
     </div>
   );
 }
---
+++ React/react-router/src/app/components/somePost.jsx
@@ -0,0 +1,5 @@
+const SomePost = () => {
+  return ( <h1>Some Post</h1> );
+}
+ 
+export default SomePost;
