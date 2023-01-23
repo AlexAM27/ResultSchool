b746b3fc8936e056710fcc5748c9907ad961c812
Alexander Markin <alexander.markin@creamteam.de>
2023-01-23 15:19:53 +0100

    route render

--- React/react-router/src/app/App.js
+++ React/react-router/src/app/App.js
@@ -13,7 +13,7 @@ function App() {
       <h1>App</h1>
       <Switch>
         <Route path="/" exact component={Home}/>
-        <Route path="/dashboard" component={Dashboard}/>
+        <Route path="/dashboard" render={(props)=>{return false && (<Dashboard isAdmin={false} {...props}/>)}}/>
         <Route path="/login" component={Login}/>
         <Route path="/posts/some-post" component={SomePost} />
         <Route path="/posts" component={Posts}/>
--- React/react-router/src/index.js
+++ React/react-router/src/index.js
@@ -6,12 +6,14 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     </BrowserRouter>
     
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
