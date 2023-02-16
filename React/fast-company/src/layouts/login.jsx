2fed6a5d9d818740c37fac0bbbbc50330b1931e9
AlexAM27 <alexandermarkin27@gmail.com>
2023-02-16 21:00:44 +0100

    validator

--- React/fast-company/src/layouts/login.jsx
+++ React/fast-company/src/layouts/login.jsx
@@ -6,9 +6,26 @@ const Login = () => {
     const [data, setData] = useState({ email: "", password: "" });
     const [errors, setErrors] = useState({});
 
+    const isValid = Object.keys(errors).length === 0;
+
     const validatorConfig = {
-        email: { isRequired: { message: "Email is required" } },
-        password: { isRequired: { message: "Password is required" } }
+        email: {
+            isRequired: { message: "Email is required" },
+            isEmail: { message: "Email is not correct" }
+        },
+        password: {
+            isRequired: { message: "Password is required" },
+            isCapitalSymbol: {
+                message: "Password should contain at least one capital letter"
+            },
+            isContainDigit: {
+                message: "Password should contain at least one digit"
+            },
+            min: {
+                message: "Password should contain min 8 symbols",
+                value: 8
+            }
+        }
     };
 
     useEffect(() => {
@@ -18,7 +35,7 @@ const Login = () => {
     const validate = () => {
         const errors = validator(data, validatorConfig);
         setErrors(errors);
-        return Object.keys(errors).length !== 0;
+        return Object.keys(errors).length === 0;
     };
 
     const handleChange = ({ target }) => {
@@ -36,24 +53,37 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <TextField
-                label="Email"
-                name="email"
-                value={data.value}
-                onChange={handleChange}
-                error={errors.email}
-            />
-            <TextField
-                label="Password"
-                type="password"
-                name="password"
-                value={data.password}
-                onChange={handleChange}
-                error={errors.password}
-            />
-            <button>Submit</button>
-        </form>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 shadow p-4">
+                    <h3 className="mb-4">Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label="Email"
+                            name="email"
+                            value={data.value}
+                            onChange={handleChange}
+                            error={errors.email}
+                        />
+                        <TextField
+                            label="Password"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            onChange={handleChange}
+                            error={errors.password}
+                        />
+                        <button
+                            type="submit"
+                            disabled={!isValid}
+                            className="btn btn-primary w-100 mx-auto"
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 };
 
