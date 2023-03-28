bf6714e6b9f97dfbbb5df389cf19af8b768c137b
AlexAM27 <alexandermarkin27@gmail.com>
2023-03-28 22:06:05 +0200

    qualities error

--- React/qualities/src/app/hooks/useQualities.jsx
+++ React/qualities/src/app/hooks/useQualities.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { toast } from "react-toastify"
 import qualityService from "../services/quality.service"
 
@@ -12,7 +12,6 @@ export const QualitiesProvider = ({ children }) => {
   const [qualities, setQualities] = useState([])
   const [error, setError] = useState(null)
   const [isLoading, setLoading] = useState(true)
-  const prevState = useRef()
   useEffect(() => {
     const getQualities = async () => {
       try {
@@ -20,8 +19,7 @@ export const QualitiesProvider = ({ children }) => {
         setQualities(content)
         setLoading(false)
       } catch (error) {
-        const { message } = error.response.data
-        setError(message)
+        errorCatcher(error)
       }
     }
     getQualities()
@@ -44,8 +42,7 @@ export const QualitiesProvider = ({ children }) => {
       )
       return content
     } catch (error) {
-      const { message } = error.response.data
-      setError(message)
+      errorCatcher(error)
     }
   }
 
@@ -55,25 +52,32 @@ export const QualitiesProvider = ({ children }) => {
       setQualities((prevState) => [...prevState, content])
       return content
     } catch (error) {
-      const { message } = error.response.data
-      setError(message)
+      errorCatcher(error)
     }
   }
 
   const deleteQuality = async (id) => {
-    prevState.current = qualities
-    setQualities((prevState) => {
-      return prevState.filter((item) => item._id !== id)
-    })
     try {
-      await qualityService.delete(id)
+      const { content } = await qualityService.delete(id)
+      setQualities((prevState) => {
+        return prevState.filter((item) => item._id !== content._id)
+      })
     } catch (error) {
-      const { message } = error.response.data
-      setError(message)
-      toast("Object not deleted")
-      setQualities(prevState.current)
+      errorCatcher(error)
     }
   }
+
+  function errorCatcher(error) {
+    const { message } = error.response.data
+    setError(message)
+  }
+
+  useEffect(() => {
+    if (error !== null) {
+      toast(error)
+      setError(null)
+    }
+  }, [error])
 
   return (
     <QualitiesContext.Provider
