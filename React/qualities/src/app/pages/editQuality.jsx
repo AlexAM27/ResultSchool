364206b748e7e5a1ba876dce4f9a757204837d45
AlexAM27 <alexandermarkin27@gmail.com>
2023-03-28 21:30:30 +0200

    hook getqualities

--- React/qualities/src/app/pages/editQuality.jsx
+++ React/qualities/src/app/pages/editQuality.jsx
@@ -1,48 +1,21 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { useParams } from "react-router-dom"
 import QualityForm from "../components/ui/qualityForm"
-import qualityService from "../services/quality.service"
-import { toast } from "react-toastify"
+import { useQualities } from "../hooks/useQualities"
 
 const EditQualityPage = () => {
-  const [quality, setQuality] = useState(null)
   const id = useParams().id
-
-  const updateQuality = async (content) => {
-    try {
-      const data = await qualityService.update(id, content)
-      return data.content
-    } catch (error) {
-      const { message } = error.response.data
-      toast.error(message)
-    }
-  }
-
-  const getQuality = async (id) => {
-    try {
-      const data = await qualityService.get(id)
-      return data.content
-    } catch (error) {
-      const { message } = error.response.data
-      toast.error(message)
-    }
-  }
+  const { getQuality, updateQuality } = useQualities()
+  const quality = getQuality(id)
 
   const handleSubmit = (data) => {
     updateQuality(data)
   }
 
-  useEffect(async () => {
-    getQuality(id).then((data) => setQuality(data))
-  }, [])
   return (
     <>
       <h1>Edit Quality Page</h1>{" "}
-      {quality !== null ? (
-        <QualityForm data={quality} onSubmit={handleSubmit} />
-      ) : (
-        "Loading..."
-      )}
+      <QualityForm data={quality} onSubmit={handleSubmit} />
     </>
   )
 }
