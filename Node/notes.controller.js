4b80a419ffdb88b5d8bb21f333892b1525b63858
AlexAM27 <alexandermarkin27@gmail.com>
2023-05-17 20:32:50 +0200

    edit note

--- Node/notes.controller.js
+++ Node/notes.controller.js
@@ -1,59 +1,57 @@
-const fs = require('fs/promises')
-const path = require('path')
-const chalk = require('chalk')
+const fs = require("fs/promises")
+const path = require("path")
+const chalk = require("chalk")
 
-const notesPath = path.join(__dirname, 'db.json')
+const notesPath = path.join(__dirname, "db.json")
 
 async function addNote(title) {
   const notes = await getNotes()
   const note = {
     title,
-    id: Date.now().toString()
+    id: Date.now().toString(),
   }
 
   notes.push(note)
   await fs.writeFile(notesPath, JSON.stringify(notes))
-  console.log(chalk.green.inverse('Note was added!'))
+  console.log(chalk.green.inverse("Note was added!"))
 }
 
 async function removeNote(id) {
   const notes = await getNotes()
 
-  const newNotes = notes.filter(note => note.id !== id)
+  const newNotes = notes.filter((note) => note.id !== id)
   await fs.writeFile(notesPath, JSON.stringify(newNotes))
-  console.log(chalk.bgRed.inverse('Note was deleted!'))
+  console.log(chalk.bgRed.inverse("Note was deleted!"))
 }
 
 async function editNote(id, title) {
   const notes = await getNotes()
-  const note = {
-    title,
-    id
-  }
 
-  console.log(typeof id)
-  const index = notes.find(note => note.id === id)
-  console.log(index)
-  notes.splice(index, 0, note)
-  console.log(notes)
+  notes.forEach((note) => {
+    if (note.id === id) {
+      note.title = title
+    }
+  })
   await fs.writeFile(notesPath, JSON.stringify(notes))
-  console.log(chalk.bgGreen.inverse('Note was changed!'))
+  console.log(chalk.bgGreen.inverse("Note was changed!"))
 }
 
 async function getNotes() {
-  const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" })
   return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
 }
 
 async function printNotes() {
   const notes = await getNotes()
-  console.log(chalk.bgBlue('Here is the list of notes:'))
-  notes.forEach(note => {
+  console.log(chalk.bgBlue("Here is the list of notes:"))
+  notes.forEach((note) => {
     console.log(chalk.blue(`${note.id} ${note.title}`))
-  });
+  })
 }
 
 module.exports = {
-  addNote, getNotes, removeNote, editNote
-
+  addNote,
+  getNotes,
+  removeNote,
+  editNote,
 }
--- Node/public/app.js
+++ Node/public/app.js
@@ -1,31 +1,38 @@
-document.addEventListener('click', (event) => {
-  if(event.target.dataset.type === 'remove') {
+document.addEventListener("click", (event) => {
+  if (event.target.dataset.type === "remove") {
     const id = event.target.dataset.id
-    remove(id).then(()=> {
-      event.target.closest('li').remove()
+    remove(id).then(() => {
+      event.target.closest("li").remove()
     })
   }
-  if(event.target.dataset.type === 'edit') {
-    const newNote = prompt('Enter new note');
-    console.log(newNote)
-    if(newNote) {
-      const id = event.target.dataset.id
-      edit(id, newNote)
+  if (event.target.dataset.type === "edit") {
+    const id = event.target.dataset.id
+
+    const noteValue = document
+      .querySelector(`[data-id='${id}']`)
+      .childNodes[0].nodeValue.trim()
+
+    const newNote = prompt("Enter new note", noteValue)
+
+    if (newNote) {
+      edit(id, newNote).then(() => {
+        event.target.closest("li").childNodes[0].nodeValue = newNote
+      })
     }
   }
 })
 
 async function edit(id, title) {
-  await fetch(`/${id}`, 
-    { method: 'PUT', 
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({title})})
+  await fetch(`/${id}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title }),
+  })
 }
 
-
 async function remove(id) {
-  await fetch(`/${id}`, {method: 'DELETE'})
+  await fetch(`/${id}`, { method: "DELETE" })
 }
