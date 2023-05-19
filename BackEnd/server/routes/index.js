917222bd6be50fd52ec0363d6c3cd504212b5790
Alexander Markin <alexander.markin@creamteam.de>
2023-05-19 12:02:06 +0200

    auth

--- BackEnd/server/routes/index.js
+++ BackEnd/server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router({mergeParams: true})
 
-router.use('/api', require('./auth.routes'))
+router.use('/auth', require('./auth.routes'))
 router.use('/comment', require('./comment.routes'))
 router.use('/quality', require('./quality.routes'))
 router.use('/profession', require('./profession.routes'))
