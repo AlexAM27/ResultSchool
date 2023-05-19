c4dfdf7b184ac4b7cbb6448ba364fe9e67b0c770
Alexander Markin <alexander.markin@creamteam.de>
2023-05-19 15:08:52 +0200

    user routes

--- BackEnd/server/routes/user.routes.js
+++ BackEnd/server/routes/user.routes.js
@@ -1,16 +1,37 @@
 const express = require('express')
+const User = require('../models/User')
+const auth = require('../middleware.js/auth.middleware')
 const router = express.Router({mergeParams: true})
 
-router.post('/signUp', async (req, res) => {
-  
+router.patch('/:userId', auth, async (req, res) => {
+  try {
+    
+    const {userId} = req.params
+
+    if (userId === req.user._id) {
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {new: true})
+      res.send(updatedUser)
+    } else {
+      res.status(401).json({message: 'Unanuthorized'})
+    }
+
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже'
+    })
+  }
 })
 
-router.post('/signInWithPassword', async (req, res) => {
-
+router.get('/', auth, async (req, res) => {
+  try {
+    const list = await User.find()
+    res.status(200).send(list)
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже'
+    })
+  }
 })
 
-router.post('/token', async (req, res) => {
-
-})
 
 module.exports = router
